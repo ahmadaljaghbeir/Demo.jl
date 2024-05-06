88c595a37eb4f73c985d6837e5aca0bab1beb486
ahmad <ahmadnajjagbeer@gmail.com>
2024-05-07 01:35:37 +0400

    ssssss

--- src/Demo.jl
+++ src/Demo.jl
@@ -1,17 +1,27 @@
-module Demo
+export add_matrices, subtract_matrices, multiply_matrices, determinant
 
-export add, subtract, multiply, divide
+"""Addition of two matrices"""
+function add_matrices(A::Matrix, B::Matrix)
+    size(A) == size(B) || throw(ArgumentError("Matrix dimensions must match"))
+    return A + B
+end
 
-"""Addition"""
-add(a, b) = a + b
+"""Subtraction of two matrices"""
+function subtract_matrices(A::Matrix, B::Matrix)
+    size(A) == size(B) || throw(ArgumentError("Matrix dimensions must match"))
+    return A - B
+end
 
-"""Subtraction"""
-subtract(a, b) = a - b
+"""Multiplication of two matrices"""
+function multiply_matrices(A::Matrix, B::Matrix)
+    size(A, 2) == size(B, 1) || throw(ArgumentError("Matrix dimensions incompatible for multiplication"))
+    return A * B
+end
 
-"""Multiplication"""
-multiply(a, b) = a * b
-
-"""Division"""
-divide(a, b) = a / b
+"""Determinant of a square matrix"""
+function determinant(A::Matrix)
+    is_square(A) || throw(ArgumentError("Matrix must be square for determinant calculation"))
+    return det(A)
+end
 
 end # module
